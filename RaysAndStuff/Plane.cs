e3f48f58384d595b35b4c11246f6147aee2ce322
RICK\rosbo <rosborne255@yahoo.com>
2023-07-05 18:13:13 -0500

    Adding original Plane class
    * Replacing the Plane class defined by System.Numerics
    with a custom implementation that implements the functions
    we need

---
+++ RaysAndStuff/Plane.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaysAndStuff
+{
+    internal class Plane
+    {
+        public Plane(Vector3 A, Vector3 B, Vector3 C)
+        {
+            Normal = Vector3.Cross((B - A), (C - A));  // NORMAL = AB X AC
+            Normal = Vector3.Normalize(Normal);      // REQUIRES NORMALIZED NORMAL
+            D = -Vector3.Dot(Normal, A);            // EVALUATE EQUATION WITH NORM AND PT ON PLANE TO GET D
+        }
+
+        //Performs a hit test between a ray and a plane
+        public bool RayIntersection(Vector3 Start, Vector3 Dir, ref float t, ref Vector3 HitPt)
+        {
+            float denom = Vector3.Dot(Normal, Dir);                   // DOT-PROD BETWEEN NORMAL AND RAY DIR
+
+            //If denom > 0 then the normal of the plane is pointing away from the ray
+            //If denom==0 RAY AND PLANE ARE PARALLEL
+            if (Math.Abs(denom) <= 1e-4f)
+                return (false);
+
+            float v0 = -(Vector3.Dot(Normal, Start) + D);
+            t = v0 / denom;                             // CALC PARAMETRIC T-VAL
+            if (t > 0)                              // IF INTERSECTION NOT "BEHIND" RAY
+            {
+                HitPt = Start + Dir * t;                // CALC HIT POINT
+                return (true);                       // INTERSECTION FOUND!
+            }
+            return (false);
+        }
+
+        public readonly Vector3 Normal;
+        public readonly float D;
+    }
+}
--- RaysAndStuff/ShootRays.cs
+++ RaysAndStuff/ShootRays.cs
@@ -18,7 +18,7 @@ namespace RaysAndStuff
             Vector3 hitPt = new Vector3();
             foreach (Plane plane in planes)
             {
-                if (RayIntersection(plane, ray.Start, ray.Dir, ref t, ref hitPt))
+                if (plane.RayIntersection(ray.Start, ray.Dir, ref t, ref hitPt))
                 {
                     Console.WriteLine("Ray hit plane:" + hitPt + " at " + t);
                 } else
@@ -37,31 +37,10 @@ namespace RaysAndStuff
             Vector3 v1 = new Vector3(-0.48f, 4.09f, 0);
             Vector3 v2 = new Vector3(0, 0, 6);
             Vector3 v3 = new Vector3(-4.3f, 0, 0);
-            Plane p1 = Plane.CreateFromVertices(v1, v2, v3);
+            Plane p1 = new Plane(v1, v2, v3);
             planes.Add(p1);
             Console.WriteLine(p1.Normal.ToString());
         }
-
-        //Performs a hit test between a ray and a plane
-        private bool RayIntersection(Plane plane, Vector3 Start, Vector3 Dir, ref float t, ref Vector3 HitPt)
-		{
-
-            float denom = Vector3.Dot(plane.Normal, Dir);					// DOT-PROD BETWEEN NORMAL AND RAY DIR
-
-			//If denom > 0 then the normal of the plane is pointing away from the ray
-			//If denom==0 RAY AND PLANE ARE PARALLEL
-			if (Math.Abs(denom)<= 1e-4f)
-				return(false);
-
-			float v0 = -(Vector3.Dot(plane.Normal, Start) + plane.D);
-            t = v0/denom;								// CALC PARAMETRIC T-VAL
-			if (t>0)								// IF INTERSECTION NOT "BEHIND" RAY
-			{
-				HitPt = Start + Dir* t;				// CALC HIT POINT
-				return(true);                       // INTERSECTION FOUND!
-			}
-			return(false);
-		}
         private List<Plane> planes = new List<Plane>();
     }
 }
