a315d3697aa9f018318bbe4d42002fba8534e7e4
RICK\rosbo <rosborne255@yahoo.com>
2023-07-06 09:35:40 -0500

    Removing obsolete code
    Removing Hello World!

--- RaysAndStuff/Program.cs
+++ RaysAndStuff/Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RaysAndStuff;
 
-Console.WriteLine("Hello, World!");
 ShootRays rayTest = new ShootRays();
 rayTest.runTest();
